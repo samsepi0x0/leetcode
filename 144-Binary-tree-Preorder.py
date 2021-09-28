b3197277cd4e1530b8fdd287335caca55b3ba9d0
samsepi0x0 <64140687+samsepi0x0@users.noreply.github.com>
2021-09-28 13:18:44 +0530

    Create 144-Binary-tree-Preorder.py

---
+++ 144-Binary-tree-Preorder.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.out = []
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        self.preorder(root)
+        return self.out
+    def preorder(self, root: Optional[TreeNode]) -> None:
+        if root is not None:
+            if root.val is not None:
+                self.out.append(root.val)
+            self.preorderTraversal(root.left)
+            self.preorderTraversal(root.right)
