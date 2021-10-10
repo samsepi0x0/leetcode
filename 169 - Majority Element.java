b145409735f7960fcccc0d39f9bf271e1938ba04
Dr. Akash <akashakxy@gmail.com>
2021-10-11 01:44:15 +0530

    Commited

---
+++ 169 - Majority Element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int element = 0;
+        int count =0;
+        for(int num  nums)
+        {
+            if(count==0)
+                element= num;
+            if(element==num)
+                count++;
+            else
+                count--;
+        }
+        return element;
+    }
+}
