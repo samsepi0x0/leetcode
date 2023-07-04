0bd0f1605e7b8869fe4978e70473be0ce976d1c0
samsepi0x0 <64140687+samsepi0x0@users.noreply.github.com>
2023-07-04 18:03:28 +0530

    Update 15_3sum.java

--- 15_3sum.java
+++ 15_3sum.java
@@ -1,19 +1,26 @@
-class Solution { // O(N*N) not understood yet.
+class Solution { // O(n*n)
     public List<List<Integer>> threeSum(int[] nums) {
-       Set<List<Integer>> res  = new HashSet<>();
-        if(nums.length==0) return new ArrayList<>(res);
-        Arrays.sort(nums);
-        for(int i=0; i<nums.length-2;i++){
-            int j =i+1;
-           int  k = nums.length-1;
-            while(j<k){
-                int sum = nums[i]+nums[j]+nums[k];
-                if(sum==0)res.add(Arrays.asList(nums[i],nums[j++],nums[k--]));
-                else if (sum >0) k--;
-                else if (sum<0) j++;
-            }
-
-        }
-        return new ArrayList<>(res);
+       Set<List<Integer>> arr = new HashSet<>();
+       if(nums.length == 0) return new ArrayList<>(arr);
+       Arrays.sort(nums);
+       for(int i = 0; i < nums.length - 2;i++){
+           int j = i+1;
+           int k = nums.length - 1;
+           //System.out.println("#" + i + "\t" + j + "\t" + k);
+           while (j < k){
+               int sum3 = nums[i] + nums[j] + nums[k];
+               if(sum3 == 0){
+                   arr.add(Arrays.asList(nums[i], nums[j++], nums[k--]));
+               }
+               else if (sum3 > 0){
+                   k--;
+               }
+               else{
+                   j++;
+               }
+               //System.out.println(i + "\t" + j + "\t" + k);
+           }
+       }
+       return new ArrayList<>(arr);
     }
 }
